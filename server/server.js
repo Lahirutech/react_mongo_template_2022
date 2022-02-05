d7c42106d0f2483a7f1806963173145c9277773a
Lanka Rathnayake <LankaR@99x.io>
2022-02-05 18:28:20 +0530

    add docker

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
-// app.use(require("./routes/record"));
+// app.use(require("./routes/record")); 
 // get driver connection
 const dbo = require("./db/connection");
+
+
+//Routes
+app.use('/users', require('./routes/users'))
 
 app.listen(port, () => {
     // perform a database connection when server starts
